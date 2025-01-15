ded5fccafae3dde5b0e11abfc7f4189619657277
Nadil Dulnidu <nadildulnidu@gmail.com>
2025-01-15 22:42:26 +0530

    make chanages

--- scripts/main.js
+++ scripts/main.js
@@ -1,23 +1,25 @@
-const todoList = JSON.parse(localStorage.getItem('todolist')) || [];
+const todoList = JSON.parse(localStorage.getItem("todolist")) || [];
 
 renderTodoList();
 
-function selectElement(selector){
+function selectElement(selector) {
   const elemet = document.querySelector(selector);
-  if (selector){
+  if (selector) {
     return elemet;
-  }else{
-    throw new Error (`Something went wrong! Make sure that ${selector} exists or typed correctly.`);
-  } 
-};
+  } else {
+    throw new Error(
+      `Something went wrong! Make sure that ${selector} exists or typed correctly.`
+    );
+  }
+}
 
-function renderTodoList(){
-  let listItemDisplay = '';
-  todoList.forEach((todoObject, id)=>{
+function renderTodoList() {
+  let listItemDisplay = "";
+  todoList.forEach((todoObject, id) => {
     const name = todoObject.name;
     const duedate = todoObject.duedate;
     const index = id;
-    if(name.length !== 0){
+    if (name.length !== 0) {
       const html = `
           <div class="list-row-item list-grid js-list-grid-${index}">
             <div class="list-items list-one">${name}</div>
@@ -36,50 +38,49 @@ function renderTodoList(){
             </div>
           </div>
         `;
-        listItemDisplay += html;  
+      listItemDisplay += html;
     }
   });
 
-  const displayElement = selectElement('.js-list-display');
+  const displayElement = selectElement(".js-list-display");
   displayElement.innerHTML = listItemDisplay;
- 
-  document.querySelectorAll('.js-delete-btn')
-  .forEach((deleteBtn, index)=>{
-    deleteBtn.addEventListener('click',()=>{
+
+  document.querySelectorAll(".js-delete-btn").forEach((deleteBtn, index) => {
+    deleteBtn.addEventListener("click", () => {
       todoList.splice(index, 1);
       renderTodoList();
       saveStorage();
     });
   });
 
-  document.querySelectorAll('.js-check')
-  .forEach((checkbtn, index)=>{
-    checkbtn.addEventListener('change', ()=>{ 
+  document.querySelectorAll(".js-check").forEach((checkbtn, index) => {
+    checkbtn.addEventListener("change", () => {
       todoList[index].completed = checkbtn.checked;
       saveStorage();
     });
     checkbtn.checked = todoList[index].completed;
   });
-};
+}
 
-const addelement = selectElement('.js-add-btn');
-addelement.addEventListener('click',()=>{addTodo();});
+const addelement = selectElement(".js-add-btn");
+addelement.addEventListener("click", () => {
+  addTodo();
+});
 
-function addTodo(){
-  const nameElement = selectElement('.js-name-input');
+function addTodo() {
+  const nameElement = selectElement(".js-name-input");
   const name = nameElement.value;
-  const dateElement = selectElement('.js-date-input');
+  const dateElement = selectElement(".js-date-input");
   const duedate = dateElement.value;
 
-  todoList.push({name: name, duedate: duedate, completed: false});
-  nameElement.value = '';
-  dateElement.value = '';
+  todoList.push({ name: name, duedate: duedate, completed: false });
+  nameElement.value = "";
+  dateElement.value = "";
 
   renderTodoList();
   saveStorage();
-};
+}
 
-function saveStorage(){
-  localStorage.setItem('todolist', JSON.stringify(todoList));
-};
-
+function saveStorage() {
+  localStorage.setItem("todolist", JSON.stringify(todoList));
+}
